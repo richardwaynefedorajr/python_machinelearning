ea0d70bc9ee5e333af721d712efa0ba39b048ec9
Rich Fedora <richardwaynefedorajr@gmail.com>
2022-10-09 15:58:51 -0400

    Added implementation for find angle problem

---
+++ hackerrank/README.md
@@ -0,0 +1,7 @@
+# Hacker rank coding challenges
+
+Collection of python implementations for coding challenges located at 
+
+## [Find angle problem](https://www.hackerrank.com/challenges/find-angle/problem?isFullScreen=true)
+
+##
---
+++ hackerrank/angle_problem.py
@@ -0,0 +1,17 @@
+from math import sin, asin, cos, atan, sqrt, degrees
+
+AB = int(input())
+BC = int(input())
+
+## right triangle tan rule
+angle_BCM = atan(AB/BC)
+
+## pythagorean theorem
+MC = 0.5*sqrt(AB**2 + BC**2)
+
+## law of cosines
+MB = sqrt(BC**2 + MC**2 - 2*BC*MC*cos(angle_BCM))
+
+## law of sines
+theta = degrees( asin( sin(angle_BCM)*MC / MB ) )
+print(str(round(theta))+chr(176))
