7b27201960ed9b8f715e3b711ba47867ff8b85ef
Rich Fedora <richardwaynefedorajr@gmail.com>
2022-10-20 04:11:47 -0400

    finished sklearn knn implementation complete with plotting

--- common_ML_algorithms/knn.py
+++ common_ML_algorithms/knn.py
@@ -1,7 +1,11 @@
+import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 ## load and split
 iris_data = load_iris()
@@ -14,7 +18,27 @@ sk_knn = KNeighborsClassifier(n_neighbors=5)
 sk_knn.fit(X_train, y_train)
 
 ## predict
-y_pred = sk_knn.predict(X_test)
+y_hat = sk_knn.predict(X_test)
 
 ## evaluate
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Accuracy:",metrics.accuracy_score(y_test, y_hat))
+
+## add data to plot
+cmap = cm.get_cmap('magma')
+fig = plt.figure()
+ax = plt.axes()
+ax.scatter(X_test[:,0], X_test[:,1], alpha=X_test[:,2]/X_test[:,2].max(), s=100*X_test[:,3], c=y_test, cmap='magma')
+ax.scatter(X_test[:,0], X_test[:,1], marker='+', c=y_hat, cmap='magma')
+ax.set_title('sklearn logistic regression\nTest data - transparency scaled based on '
+             +iris_data.feature_names[2]+' and size scaled based on '+iris_data.feature_names[3]+'\n'
+             +str(round(np.mean(y_hat == y_test)*100,2))+'% accuracy')
+ax.set_xlabel(iris_data.feature_names[0])
+ax.set_ylabel(iris_data.feature_names[1])
+legend_elements = [Line2D([0], [0], marker='o', color='w', label=iris_data.target_names[0], markerfacecolor=cmap(0), markersize=10),
+                   Line2D([0], [0], marker='o', color='w', label=iris_data.target_names[1], markerfacecolor=cmap(127), markersize=10),
+                   Line2D([0], [0], marker='o', color='w', label=iris_data.target_names[2], markerfacecolor=cmap(255), markersize=10),
+                   Line2D([0], [0], marker='P', color='w', label=iris_data.target_names[0]+' predicted', markerfacecolor=cmap(0), markersize=10),
+                   Line2D([0], [0], marker='P', color='w', label=iris_data.target_names[1]+' predicted', markerfacecolor=cmap(127), markersize=10),
+                   Line2D([0], [0], marker='P', color='w', label=iris_data.target_names[2]+' predicted', markerfacecolor=cmap(255), markersize=10)]
+ax.legend(handles=legend_elements, loc='best', ncol=2)
+plt.show()
