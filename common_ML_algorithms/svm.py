7dac8f69a23997b4550e9e5c081e7218b6be3b90
Rich Fedora <richardwaynefedorajr@gmail.com>
2022-10-20 03:25:44 -0400

    added some plotting to svm.py

--- common_ML_algorithms/svm.py
+++ common_ML_algorithms/svm.py
@@ -1,24 +1,30 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn import metrics
+import tensorflow as tf
 
 ## import data
 cancer = datasets.load_breast_cancer()
-X_train, X_test, y_train, y_test = train_test_split(cancer.data,
-                                                    cancer.target,
-                                                    test_size=0.3,
-                                                    random_state=109)
+cancer_df = pd.DataFrame(np.c_[cancer['data'], cancer['target']], columns= np.append(cancer['feature_names'], ['target']))
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.3, random_state=109)
 
 ## train
-clf = svm.SVC(kernel='linear').fit(X_train, y_train)
+sk_svm = svm.SVC(kernel='linear').fit(X_train, y_train)
 
 ## predict
 y_hat = clf.predict(X_test)
 
-# accuracy
+## accuracy
 print("Accuracy:",metrics.accuracy_score(y_test, y_hat))
 print("Precision:",metrics.precision_score(y_test, y_hat))
 print("Recall:",metrics.recall_score(y_test, y_hat))
+
+## plotting
+#sns.pairplot(cancer_df, hue='target', vars=cancer.feature_names[0:3])
+#plt.figure(figsize=(20,12))
+#sns.heatmap(cancer_df.corr(), annot=True)
