498df20f6bb01743c8bb0cf72dd793b700d69a2f
Rich Fedora <richardwaynefedorajr@gmail.com>
2022-10-20 04:00:37 -0400

    initial commit knn

---
+++ common_ML_algorithms/knn.py
@@ -0,0 +1,20 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+## load and split
+iris_data = load_iris()
+X_train, X_test, y_train, y_test = train_test_split( iris_data.data, iris_data.target, test_size = 0.3, random_state = 100 )
+
+## create model
+sk_knn = KNeighborsClassifier(n_neighbors=5)
+
+## train
+sk_knn.fit(X_train, y_train)
+
+## predict
+y_pred = sk_knn.predict(X_test)
+
+## evaluate
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
