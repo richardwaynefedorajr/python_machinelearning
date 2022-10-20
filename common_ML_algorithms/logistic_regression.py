47f3c7f55e2bbd5a607694d7c886aa0ec2c82d86
Rich Fedora <richardwaynefedorajr@gmail.com>
2022-10-20 04:17:55 -0400

    fixed bug -> plotted predicted values were actually test values

--- common_ML_algorithms/logistic_regression.py
+++ common_ML_algorithms/logistic_regression.py
@@ -24,17 +24,17 @@ cmap = cm.get_cmap('viridis')
 fig = plt.figure()
 ax = plt.axes()
 ax.scatter(X_test[:,0], X_test[:,1], alpha=X_test[:,2]/X_test[:,2].max(), s=100*X_test[:,3], c=y_test, cmap='viridis')
-ax.scatter(X_test[:,0], X_test[:,1], marker='+', c=y_test, cmap='viridis')
+ax.scatter(X_test[:,0], X_test[:,1], marker='+', c=y_hat, cmap='viridis')
 ax.set_title('sklearn logistic regression\nTest data - transparency scaled based on '
              +iris_data.feature_names[2]+' and size scaled based on '+iris_data.feature_names[3]+'\n'
              +str(round(np.mean(y_hat == y_test)*100,2))+'% accuracy')
 ax.set_xlabel(iris_data.feature_names[0])
 ax.set_ylabel(iris_data.feature_names[1])
 legend_elements = [Line2D([0], [0], marker='o', color='w', label=iris_data.target_names[0], markerfacecolor=cmap(0), markersize=10),
-                   Line2D([0], [0], marker='o', color='w', label=iris_data.target_names[1], markerfacecolor=cmap(127), markersize=10),
-                   Line2D([0], [0], marker='o', color='w', label=iris_data.target_names[2], markerfacecolor=cmap(255), markersize=10),
-                   Line2D([0], [0], marker='P', color='w', label=iris_data.target_names[0]+' predicted', markerfacecolor=cmap(0), markersize=10),
-                   Line2D([0], [0], marker='P', color='w', label=iris_data.target_names[1]+' predicted', markerfacecolor=cmap(127), markersize=10),
-                   Line2D([0], [0], marker='P', color='w', label=iris_data.target_names[2]+' predicted', markerfacecolor=cmap(255), markersize=10)]
+                    Line2D([0], [0], marker='o', color='w', label=iris_data.target_names[1], markerfacecolor=cmap(127), markersize=10),
+                    Line2D([0], [0], marker='o', color='w', label=iris_data.target_names[2], markerfacecolor=cmap(255), markersize=10),
+                    Line2D([0], [0], marker='P', color='w', label=iris_data.target_names[0]+' predicted', markerfacecolor=cmap(0), markersize=10),
+                    Line2D([0], [0], marker='P', color='w', label=iris_data.target_names[1]+' predicted', markerfacecolor=cmap(127), markersize=10),
+                    Line2D([0], [0], marker='P', color='w', label=iris_data.target_names[2]+' predicted', markerfacecolor=cmap(255), markersize=10)]
 ax.legend(handles=legend_elements, loc='best', ncol=2)
 plt.show()
