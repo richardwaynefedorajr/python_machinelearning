36b0a60d350f91784572856585c2c9cfaaaa3678
Rich Fedora <richardwaynefedorajr@gmail.com>
2022-11-16 23:55:18 -0500

    re-factor directory tree due to expanded number of artifacts, update plotting for KNN

--- common_ML_algorithms/README.md
+++ common_ML_algorithms/README.md
@@ -1,11 +1,19 @@
+# Common machine learning algorithms
+
 Various implementations of common machine learning algorithms
 
-# Linear Regression
+## Linear Regression
 Closed form solution compared to tensorflow implementation:
 
-![Example linear regression output](./linear_regression.png)
+![Example linear regression output](./linear_regression/linear_regression.png)
 
-# Logistic Regression
+## Logistic Regression
 scikit-learn implementation using iris dataset
 
-![Logistic regression classification of iris dataset](./sklearn_logistic_regression.png)
+![Logistic regression classification of iris dataset](./logistic_regression/sklearn_logistic_regression.png)
+
+## K-nearest neighbors
+
+sci-kit learn implementation using iris dataset
+
+![Histograms](./knn/knn_histograms.png) ![Correlation matrix](./knn/knn_corr_matrix.png) ![Pair plots](./knn/knn_pairplots.png)
---
+++ common_ML_algorithms/knn/knn.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+## load and split
+iris_data = load_iris()
+X_train, X_test, y_train, y_test = train_test_split( iris_data.data, iris_data.target, test_size = 0.3, random_state = 100 )
+
+## create model
+sk_knn = KNeighborsClassifier(n_neighbors=5)
+
+## train
+sk_knn.fit(X_train, y_train)
+
+## predict
+y_hat = sk_knn.predict(X_test)
+
+## evaluate
+print("Accuracy:",metrics.accuracy_score(y_test, y_hat))
+
+## create dataframes for visualization
+iris_df = pd.DataFrame(data=np.c_[iris_data['data'], iris_data['target']], columns=iris_data['feature_names'] + ['target'])
+
+## histograms
+fig, axs = plt.subplots(2, 2, figsize=(7, 7))
+sns.histplot(data=iris_df, x=iris_data.feature_names[0], bins=50, color="chartreuse", ax=axs[0, 0])
+sns.histplot(data=iris_df, x=iris_data.feature_names[1], bins=50, color="lawngreen", ax=axs[0, 1])
+sns.histplot(data=iris_df, x=iris_data.feature_names[2], bins=50, color="green", ax=axs[1, 0])
+sns.histplot(data=iris_df, x=iris_data.feature_names[3], bins=50, color="lime", ax=axs[1, 1])
+plt.show()
+plt.savefig('knn_histograms.png')
+
+## correlation matrix
+ax = sns.heatmap(iris_df[iris_data.feature_names].corr(), cmap="Greens", annot=True, square=True)
+ax.set_ylim(0, len(iris_df[iris_data.feature_names].corr()))
+plt.show()
+plt.savefig('knn_corr_matrix.png')
+
+## pairplots
+iris_df['target'][iris_df["target"] == 0.0] = iris_data.target_names[0]
+iris_df['target'][iris_df["target"] == 1.0] = iris_data.target_names[1]
+iris_df['target'][iris_df["target"] == 2.0] = iris_data.target_names[2]
+sns.pairplot(iris_df, hue="target")
+plt.show()
+plt.savefig('knn_pairplots.png')
