f40477c5921268fbe6e0647459a3f9069715da41
Rich Fedora <richardwaynefedorajr@gmail.com>
2023-01-12 20:12:09 -0500

    finished softmax/MLP/CNN comparison on MNIST Fashion dataset

--- deep_learning/README.md
+++ deep_learning/README.md
@@ -12,12 +12,43 @@ Visualizations generated from pytorch and tensorflow implementations.
 |:--:| 
 | *Tensorflow linear regression output* |
 
-## Softmax Classifier
-| ![pytorch softmax regression output](./softmax_regression/softmax_regression_torch.svg) |
-|:--:| 
-| *Pytorch softmax classifier mis-labeled images* |
+## MNIST Fashion Dataset - Softmax Classifier, MLP, and CNN
 
-## MLP Classifier
-| ![tensorflow_MLP output](./MLP/MLP_tf.svg) |
+Comparison of classification results on MNIST Fashion dataset between pytorch softmax classifier, tensorflow MLP, and pytorch CNN.
+
+First, the softmax results:
+
+| ![pytorch softmax regression metrics](./softmax_regression/pytorch_softmax_metrics.svg) |
 |:--:| 
-| *Tensorflow MLP classifier mis-labeled images* |
+| *Pytorch softmax classifier training metrics* |
+
+| ![pytorch softmax regression output](./softmax_regression/softmax_confusion_matrix.svg) |
+|:--:| 
+| *Pytorch softmax classifier results* |
+
+Let us extend this line of action (switching to tensorflow for implementation), and add a hidden layer...
+
+| ![tensorflow_MLP metrics](./MLP/tensorflow_MLP_metrics.svg) |
+|:--:| 
+| *Tensorflow MLP training metrics* |
+
+| ![tensorflow_MLP output](./MLP/tensorflow_MLP_matrix.svg) |
+|:--:| 
+| *Tensorflow MLP results* |
+
+
+Finally, a CNN implementation in pytorch (generally follows LeNet architecture, but substititutes relu activation function and adds batch normalization)
+
+| ![pytorch CNN metrics](./CNN/pytorch_CNN_metrics.svg) |
+|:--:| 
+| *Pytorch CNN training metrics* |
+
+| ![pytorch CNN output](./CNN/CNN_confusion_matrix.svg) |
+|:--:| 
+| *Pytorch CNN results* |
+
+Displayed below are examples of mis-labeled elements of the validation dataset:
+
+| ![pytorch CNN mislabeled](./CNN/CNN_mislabeled.svg) |
+|:--:| 
+| *Pytorch CNN mis-labeled image examples* |
--- deep_learning/softmax_regression/softmax_regression_pytorch.py
+++ deep_learning/softmax_regression/softmax_regression_pytorch.py
@@ -1,60 +1,50 @@
 import torch
 import torchvision
+import torchmetrics
+
 from torchvision import transforms
 from torch import nn
 from torch.nn import functional as F
+from torchmetrics import ConfusionMatrix
+
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-def getLabelsFromIndex(labels):
-    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
-    return [text_labels[int(i)] for i in labels]
+sns.set(rc={"figure.dpi":600, 'savefig.dpi':600})
 
-def setSubplotDimensions(num_subplots):
-    min_cols = 6
-    max_cols = 10
-    possible_num_cols = np.arange(min_cols,max_cols+1)
-    num_cols = possible_num_cols[np.argmin(num_subplots % possible_num_cols)]
-    if num_subplots < max_cols:
-        return 1, num_subplots
-    elif num_subplots % num_cols == 0:
-        return num_subplots // num_cols, num_cols
-    else:
-        num_cols = possible_num_cols[np.argmax(num_subplots % possible_num_cols)]
-        return num_subplots // num_cols + 1, num_cols
+def plotTrainingMetrics(loss, accuracy, epochs):
+    fig, ax = plt.subplots()
+    ax.plot(range(epochs), loss/loss.max(), label='Training loss')
+    ax.plot(range(epochs), accuracy, label='Training accuracy')
+    ax.set(xlabel='Epochs', ylabel='Metrics', title='Pytorch Softmax Training Accuracy and Loss')
+    ax.grid()
+    ax.legend()
+    fig.savefig("pytorch_softmax_metrics.svg")
+    plt.show()
+
+def plotHeatmap(confusion_matrix, accuracy, ax, method):
+    labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    sns.heatmap(confusion_matrix/np.sum(confusion_matrix), annot=True, fmt=".2%", linewidths=.5, square = True, cmap = 'Greens',
+            xticklabels=labels, yticklabels=labels, ax=ax, cbar_kws={"shrink": 0.75}, annot_kws={'fontsize':30})
+    ax.xaxis.set_tick_params(labelsize = 18)
+    ax.yaxis.set_tick_params(labelsize = 18)
+    ax.set_ylabel('Actual label', fontsize=24)
+    ax.set_xlabel('Predicted label', fontsize=24)
+    ax.set_title('{} Accuracy: {}%'.format(method, round(accuracy*100, 2)), fontsize=36)
     
-def show_images(imgs, num_subplots, titles=None, scale=1.5, accuracy=0.0):
-    label_fontsize, title_fontsize, colormap = 18, 24, 'viridis'
-    num_rows, num_cols = setSubplotDimensions(num_subplots)
-    figsize = (num_cols * scale, num_rows * scale**2)
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
-    axes = axes.flatten()
-    for ax in axes:
-        ax.set_axis_off()
-    for i, (ax, img) in enumerate(zip(axes, imgs)):
-        try:
-            img = img.detach().numpy()
-        except:
-            pass
-        ax.imshow(img, cmap=colormap)
-
-        if titles:
-            ax.set_title(titles[i], fontsize=label_fontsize)
-            
-    fig.suptitle('Softmax regression on MNIST Fashion dataset: Accuracy = {}%'.format(round(accuracy*100,2)), 
-                 fontsize=title_fontsize)
-    plt.tight_layout()
-    plt.savefig('softmax_regression_torch.svg', bbox_inches='tight')
-    return axes
-
 def softmax(X):
     return torch.exp(X) / X_exp.sum(1, keepdims=True)
 
 # hyperparameters
 batch_size = 256
 num_outputs = 10
-lr = 0.25
+lr = 0.0025
 epochs = 10
+
+cm = ConfusionMatrix(task='multiclass', num_classes=num_outputs)
+training_loss = np.empty(epochs)
+training_accuracy = np.empty(epochs)
 
 # load data
 trans = transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor()])
@@ -74,14 +64,16 @@ for epoch in range(epochs):
         loss.backward()
         optimizer.step()
     print('epoch {}, loss {}'.format(epoch, loss.item()))
+    training_loss[epoch] = loss.item()
+    training_accuracy[epoch] = np.mean(y_hat.argmax(axis=1).numpy() == y.numpy())
 
 # predict
 X, y = next(iter(torch.utils.data.DataLoader(val, batch_size=batch_size, shuffle=True, num_workers=0)))
 y_hat = model(X).argmax(axis=1)
 
 # evaluate
-wrong = y_hat != y
-X, y, y_hat = X[wrong], y[wrong], y_hat[wrong]
-accuracy = 1 - len(y_hat)/batch_size
-labels = [a+'\n'+b for a, b in zip(getLabelsFromIndex(y), getLabelsFromIndex(y_hat))]
-show_images(X.squeeze(1), len(y_hat), scale=1.5, titles=labels, accuracy=accuracy)
+plotTrainingMetrics(training_loss, training_accuracy, epochs)
+confusion_matrix_val = cm(y_hat, y)
+fig, ax = plt.subplots(figsize=(30,30))
+plotHeatmap(confusion_matrix_val.numpy(), np.mean(y_hat.numpy() == y.numpy()), ax, 'Softmax Regression Validation Results')
+plt.savefig('softmax_confusion_matrix.svg', bbox_inches='tight')
