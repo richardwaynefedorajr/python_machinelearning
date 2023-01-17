46fcbb1e689f363bf34b5fc88ca30b3dd687b4aa
Rich Fedora <richardwaynefedorajr@gmail.com>
2023-01-17 15:35:09 -0500

    update tensorflow linear regression to inherit from utility model class, refactor model classes, implement sequential linear regression on sythetic data

--- deep_learning/linear_regression/linear_regression_tf.py
+++ deep_learning/linear_regression/linear_regression_tf.py
@@ -1,50 +1,50 @@
+import sys
+sys.path.append('../utils')
+from model import ModelTF
+
 import random
-import numpy as np
-import pandas as pd
 import tensorflow as tf
+
 from tensorflow import keras
 from tensorflow.keras import layers
-import matplotlib.pyplot as plt
-import matplotlib
-import seaborn as sns
-sns.set(rc={"figure.dpi":600, 'savefig.dpi':600})
 
-# hyperparameters
+class LinearRegression(ModelTF):
+    def __init__(self, batch_size=256, lr=0.1, epochs=10, labels=None):
+        super().__init__(batch_size, lr, epochs, labels)
+        self.output_dimension = 1
+        
+        self.model = tf.keras.Sequential(layers.Dense(units=self.output_dimension))
+        self.optimizer = tf.keras.optimizers.SGD(learning_rate=self.lr)
+        self.loss_function = 'mean_squared_error'
+        
+        self.file_prefix = 'tensorflow_linear_regression'
+
+    def predict(self, X, y):
+        self.X = X
+        self.y = y[:self.batch_size]
+        self.y_hat = self.model.predict(X[:self.batch_size])
+    
+    def plotLinearRegression(self, title):
+        super().scatterPlot(title)
+        
+# generate data
 w = tf.constant([-3.2]) #, -3.4])
 b = 6.4
 noise = 0.5
 num_samples = 100
-
-# generate data
 X = tf.random.normal((num_samples, w.shape[0]))
 noise = tf.random.normal((num_samples, 1)) * noise
 y = tf.matmul(X, tf.reshape(w, (-1, 1))) + b + noise
 
 # create model
-tf_model = tf.keras.Sequential(layers.Dense(units=1))
+model = LinearRegression(batch_size=num_samples, lr=0.1, epochs=250)
+model.compile()
 
-# loss function and optimizer
-tf_model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.1), loss='mean_squared_error')
-
-# fit
-tf_model.fit(X, y, epochs=250, verbose=0, validation_split=0.2)
+# train
+model.fit(X, y)
 
 # predict
-y_hat = tf_model.predict(X)
+model.predict(X, y)
 
 # evaluate
-squared_error = np.square(y - y_hat)
-
-## add data to plot
-fig = plt.figure()
-ax = plt.axes()
-df = pd.DataFrame(data=np.c_[X[:,0], y, y_hat], columns=['X', 'y', 'y_hat'])
-sns.scatterplot(data=df, x='X', y='y', ax=ax, edgecolor='black', size=np.abs(y-y_hat)[:,0], c='greenyellow')
-# sns.scatterplot(data=df, x='X', y='y_hat', ax=ax, marker='o', edgecolor='black', s=2, c='darkgreen')
-ax.plot(X[:,0], y_hat, color='green', linestyle='solid', linewidth=1, label='y_hat')
-ax.set_title('Tensorflow linear regression [MSE = '+str(round(squared_error.mean(),2))+']')
-ax.set_xlabel("x")
-ax.set_ylabel("y")
-ax.legend(fontsize=8, title_fontsize=8, title='marker size for\nerror values')
-plt.savefig('linear_regression_tensorflow.svg', bbox_inches='tight')
-plt.show()
+model.plotLinearRegression('Tensorflow linear regression')
--- deep_learning/utils/model.py
+++ deep_learning/utils/model.py
@@ -3,6 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import tensorflow as tf
+import numpy as np
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.utils import np_utils
+from keras.utils import plot_model
 from sklearn.metrics import confusion_matrix
 
 class Model():
@@ -27,16 +34,30 @@ class Model():
         
         self.file_prefix  = None
         
-    def linePlot(self, data, title):
+    def linePlot(self, data, title, linewidth=0.75):
         sns.set(rc={"figure.dpi":600, 'savefig.dpi':600})
         vals = np.linspace(0,1,256)
         np.random.shuffle(vals)
         cmap = plt.cm.colors.ListedColormap(plt.cm.gist_rainbow(vals))
         sns.set_palette(cmap(np.linspace(0,1,cmap.N)))
         sns.set_style('darkgrid', {'axes.facecolor':'0.85'})
-        plot = sns.lineplot(data, dashes=False, linewidth=0.75)
+        plot = sns.lineplot(data, dashes=False, linewidth=linewidth)
         plot.figure.suptitle(title)
         return plot
+    
+    def scatterPlot(self, title):
+        fig = plt.figure()
+        ax = plt.axes()
+        df = pd.DataFrame(data=np.c_[self.X[:,0], self.y, self.y_hat], columns=['X', 'y', 'y_hat'])
+        accuracy = round(np.mean(np.square(self.y_hat - self.y)), 2)
+        sns.scatterplot(data=df, x='X', y='y', ax=ax, edgecolor='black', size=np.abs(self.y-self.y_hat)[:,0], c='greenyellow')
+        ax.plot(self.X[:,0], self.y_hat, color='green', linestyle='solid', linewidth=1, label='y_hat')
+        ax.set_title(title+' [MSE = '+str(accuracy)+']')
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.legend(fontsize=8, title_fontsize=8, title='marker size for\nerror values')
+        plt.savefig(self.file_prefix+'_scatterplot.svg', bbox_inches='tight')
+        plt.show()
     
     def heatmap(self, ax, title, data):
         sns.set(rc={"figure.dpi":600, 'savefig.dpi':600})
@@ -62,3 +83,28 @@ class Model():
         title = self.file_prefix+' Confusion Matrix: Accuracy = {}'.format(round(accuracy*100, 2))
         ax = self.heatmap(ax, title, self.confusion_matrix/self.confusion_matrix.max())
         plt.savefig(self.file_prefix+'_confusion_matrix.svg', bbox_inches='tight')
+        
+class ModelTF(Model):
+    def __init__(self, batch_size=256, lr=0.1, epochs=10, labels=None):
+        super().__init__(batch_size, lr, epochs, labels)
+        
+    def compile(self):
+        self.model.compile(optimizer=self.optimizer, loss=self.loss_function, metrics=['accuracy'])
+        
+    def fit(self, X, y):
+        self.history = self.model.fit(X, y, epochs=self.epochs, verbose=1, batch_size=self.batch_size, shuffle=True)
+        self.training_accuracy = np.array(self.history.history['accuracy'])
+        self.training_loss = np.array(self.history.history['loss'])
+        
+    def predict(self, X, y):
+        self.y = y[:self.batch_size]
+        self.y_hat = self.model.predict(X[:self.batch_size]).argmax(axis=1)
+
+    def plotConfusionMatrix(self):
+        super().plotConfusionMatrix(self.y, self.y_hat)
+        
+    def showNetworkStructure(self):
+        plot_model(self.model, to_file=self.file_prefix+'_structure.png', show_shapes=True)
+
+class ModelTorch(Model):
+    pass
